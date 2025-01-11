0ec497218274f4acf4994bf2abfacbafde83e272
OinkClan <loinkgd@gmail.com>
2025-01-11 14:34:23 -0500

    Create wordData.js

---
+++ wordData.js
@@ -0,0 +1,22 @@
+const wordData = [
+    { 
+        word: "Aouki", 
+        type: "noun", 
+        definitions: ["A person of great wisdom.", "A title for elders in Oinkish culture."]
+    },
+    { 
+        word: "Eroi", 
+        type: "verb", 
+        definitions: ["To speak eloquently.", "To persuade others with words."]
+    },
+    { 
+        word: "Ounato", 
+        type: "adjective", 
+        definitions: ["Bright, shining.", "Used to describe an illuminated object."]
+    },
+    { 
+        word: "Ure", 
+        type: "particle", 
+        definitions: ["Used to indicate future tense.", "An auxiliary particle."]
+    }
+];
